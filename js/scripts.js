f8a908a7169ee2fa6a7dc9fa34ef31d6c37a71e6
Daniel Hongel and Jacob Irwin <daniel@>
2019-06-12 11:17:55 -0700

    add if statement to evaluate for letter y and convert to pig latin. Also add code to convert user input to lower case.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
+var pigLatin = '';
+function translate(input) {
+  var inputArr = input.split(" ");
+  // var vowels = ['a','e','i','o','u'];
+  console.log(inputArr);
 
+    for(i=0;i<inputArr.length;i++){
+        var letterArr = inputArr[i].split('');
+        console.log(letterArr);
+
+        if(letterArr[i] === "y") {
+          letterArr.splice(0,1);
+          letterArr.push("y", "a", "y");
+          var latinWord = letterArr.join('');
+          pigLatin = pigLatin + latinWord;
+          console.log(latinWord);
+        }
+    };
+  };
 //////////////////////////////
 $(document).ready(function() {
   $("form#pigLatin").submit(function(event) {
     event.preventDefault();
-    var input = $("input#latin").val();
+    var input = ($("input#latin").val()).toLowerCase();
     var latin = translate(input);
-    console.log(input);
     $("#result").text(latin);
   });
 });
